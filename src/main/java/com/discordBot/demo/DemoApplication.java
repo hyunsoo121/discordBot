6c5bef8e234f4df8a07894fb1aef7e84b745457d
ggom121@naver.com <ggom121@naver.com>
2025-10-01 10:32:58 +0900

    feat: DiscordBotConfig 작성

--- src/main/java/com/discordBot/demo/DemoApplication.java
+++ src/main/java/com/discordBot/demo/DemoApplication.java
@@ -1,5 +1,10 @@
 package com.discordBot.demo;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +14,4 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
