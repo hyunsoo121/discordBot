720f922be330e3b3569cba9ab42594c5596a0053
ggom121@naver.com <ggom121@naver.com>
2025-10-01 17:55:12 +0900

    feat: 채팅 반응 구현

--- src/main/java/com/discordBot/demo/config/DiscordBotConfig.java
+++ src/main/java/com/discordBot/demo/config/DiscordBotConfig.java
@@ -1,5 +1,6 @@
 package com.discordBot.demo.config;
 
+import com.discordBot.demo.listener.DiscordBotListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -8,33 +9,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.security.auth.login.LoginException;
-
 @Configuration
 public class DiscordBotConfig {
 
     @Value("${spring.discord.bot.token}")
     private String token;
 
-    // 2. JDA 객체를 Spring Bean으로 등록
     @Bean
-    public JDA discordJDA() throws LoginException, InterruptedException {
+    public JDA discordJDA() throws InterruptedException {
         JDA jda = JDABuilder.createDefault(token)
                 .setActivity(Activity.playing("메세지 기다리는중!"))
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .addEventListeners(new DiscordBotListener())
+                .addEventListeners(new)
                 .build();
 
-        // JDA가 완전히 로드될 때까지 기다림
          jda.awaitReady();
 
         return jda;
     }
-
-    // 3. 만약 이벤트 리스너가 있다면 여기서 JDA에 추가할 수 있습니다.
-    /*
-    @Bean
-    public void addEventListeners(JDA jda, MyEventListener listener) {
-        jda.addEventListener(listener);
-    }
-    */
 }
---
+++ src/main/java/com/discordBot/demo/listener/DiscordBotListener.java
@@ -0,0 +1,61 @@
+package com.discordBot.demo.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.Arrays;
+
+@Slf4j
+public class DiscordBotListener extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        User user = event.getAuthor();
+        TextChannel textChannel = event.getChannel().asTextChannel();
+        Message message = event.getMessage();
+
+        log.info(" get message :" + message.getContentDisplay());
+
+        if (user.isBot()){
+            return;
+        }
+        else if (message.getContentDisplay().equals("")){
+            log.info("디스코드 message 문자열 값 공백");
+        }
+
+        String[] messageArray = message.getContentDisplay().split(" ");
+
+        if (messageArray[0].equalsIgnoreCase("봇아")) {
+
+
+            String[] messageArgs = Arrays.copyOfRange(messageArray, 1, messageArray.length);
+
+            for (String msg : messageArgs){
+                String returnMessage = sendMessage(event, msg);
+                textChannel.sendMessage(returnMessage).queue();
+            }
+        }
+
+    }
+
+    private String sendMessage(MessageReceivedEvent event, String message){
+        User user = event.getAuthor();
+        String returnMessage;
+
+        switch (message){
+            case "test" : returnMessage = user.getName() + "테스트 중이세요??";
+            break;
+            case "안녕" : returnMessage = user.getName() + "님 안녕하세요!";
+            break;
+            case "누구야" : returnMessage = user.getAsMention() + "님 저는 뭐 네.";
+            break;
+            default: returnMessage = "등록되지 않은 명령어입니다.";
+            break;
+        }
+
+        return returnMessage;
+    }
+}
