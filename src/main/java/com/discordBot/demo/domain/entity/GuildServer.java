6410d13556e07cb44e2880f92027ea9dbe3cac76
ggom121@naver.com <ggom121@naver.com>
2025-10-14 13:13:13 +0900

    feat: UserServiceImplTest 추가

--- src/main/java/com/discordBot/demo/domain/entity/GuildServer.java
+++ src/main/java/com/discordBot/demo/domain/entity/GuildServer.java
@@ -1,6 +1,7 @@
 package com.discordBot.demo.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor; // NoArgsConstructor 추가
