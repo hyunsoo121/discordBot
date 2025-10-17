1eb7387a059fc06e8734032260a9e90da6e0ffd3
ggom121@naver.com <ggom121@naver.com>
2025-10-17 20:30:15 +0900

    feat: playerStatsDto 필드 추가

--- src/main/java/com/discordBot/demo/domain/dto/PlayerStatsDto.java
+++ src/main/java/com/discordBot/demo/domain/dto/PlayerStatsDto.java
@@ -1,7 +1,6 @@
 package com.discordBot.demo.domain.dto;
 
 import lombok.*;
-// ⭐ Jackson 어노테이션 import 추가
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Data
@@ -9,17 +8,17 @@ public class PlayerStatsDto {
 
     private Long discordUserId;
 
-    // ⭐ AI가 보내는 필드 이름(gameName)을 DTO 필드(lolGameName)에 매핑
     @JsonProperty("gameName")
     private String lolGameName;
 
-    // ⭐ AI가 보내는 필드 이름(tagLine)을 DTO 필드(lolTagLine)에 매핑
     @JsonProperty("tagLine")
     private String lolTagLine;
 
-    // 이 필드들은 이름이 일치하므로 @JsonProperty("team")는 생략 가능
     private String team;
-    private Integer kills;
-    private Integer deaths;
-    private Integer assists;
+    private int kills;
+    private int deaths;
+    private int assists;
+
+    private int totalGold;
+    private int totalDamage;
 }
